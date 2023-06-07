faf06bada0a1fa03681f3f3dd4023d340288b6db
tiko.nats <tiko.nats@icloud.com>
2023-06-07 01:42:52 -0700

    handler for metrics value

--- cmd/server/handler.go
+++ cmd/server/handler.go
@@ -1,33 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 )
 
 func PostHandler(res http.ResponseWriter, req *http.Request) {
-	//Обработчик, что пост
 	methodTypeHandler(res, req)
-	//Обработчик, что не правильный path
-	//Обработчик, что не правильный тип контента
-	//Обработчик, что правильный URL
-	//Обработчик<ТИП_МЕТРИКИ>/
+
 	relPathString := req.URL.Path
 	pathParamsArray := strings.Split(relPathString, "/")
 	if len(pathParamsArray) != 5 {
 		http.Error(res, "incorrect request", http.StatusNotFound)
 	}
+
 	metricsType := pathParamsArray[2]
 	checkType(res, metricsType)
-	//Обработчик<ИМЯ_МЕТРИКИ>
 	//metricsName := pathParamsArray[3]
 	//обработчик <ЗНАЧЕНИЕ_МЕТРИКИ>
+	metricsValue := pathParamsArray[4]
+	checkMetricsValue(res, metricsValue)
+}
 
-	body := req.Method
-
-	for k, v := range req.Header {
-		body += fmt.Sprintf("%s: % v\r\n", k, v)
+func checkMetricsValue(res http.ResponseWriter, metricValue string) {
+	_, e := strconv.Atoi(metricValue)
+	if e != nil {
+		http.Error(res, "incorrect value of metrics", http.StatusBadRequest)
+		return
 	}
 }
 
